b2c58db4dddf2090fb28c478fc4791c7443af1e4
青橘 <qingju@qingjudeMacBook-Pro.local>
2024-04-07 13:19:34 +0800

    ceshi

--- pages/bill/views/home/index.vue
+++ pages/bill/views/home/index.vue
@@ -2,7 +2,7 @@
  * @Author       : 桔子
  * @Date         : 2024-03-26 18:56:14
  * @LastEditors  : 桔子
- * @LastEditTime : 2024-04-07 11:43:09
+ * @LastEditTime : 2024-04-07 13:19:28
  * @Description  : 头部注释配置模板
  * @FilePath     : /vue-multipage/pages/bill/views/home/index.vue
 -->
@@ -11,5 +11,5 @@ defineProps<{ msg: string }>()
 </script>
 
 <template>
-    <div> {{  'hello world Bill 测试'  }} </div>
+    <div> {{  'hello world Bill'  }} </div>
 </template>
